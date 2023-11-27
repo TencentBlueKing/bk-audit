7a4c08bb3682aa0e9f09f83eea7a3af9d37f2eee
v_yyhoyang <nanasikeai@gmail.com>
2023-11-27 16:34:33 +0800

    feat: 企业版屏蔽BKVision特性

--- src/frontend/src/domain/service/statement-manage.ts
+++ src/frontend/src/domain/service/statement-manage.ts
@@ -26,4 +26,8 @@ export default {
     })
       .then(({ data }) => data);
   },
+  fetchHasBkvision() {
+    return StatementManageSource.getHasBkvision()
+      .then(({ data }) => data);
+  },
 };
