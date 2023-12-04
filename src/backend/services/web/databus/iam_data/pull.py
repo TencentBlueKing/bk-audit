2d00990170f26c73ff39657b85e80ffc0f3108cf
orenzhang <orenzhang@ovinc.cn>
2023-12-04 12:23:34 +0800

    chore: puller token init

--- src/backend/services/web/databus/iam_data/pull.py
+++ src/backend/services/web/databus/iam_data/pull.py
@@ -19,6 +19,7 @@ to the current version of the project delivered to anyone in the future.
 import datetime
 import json
 import os
+import uuid
 
 from bk_resource import api
 from bk_resource.settings import bk_resource_settings
@@ -54,9 +55,9 @@ class HttpPullHandler:
             )
             self.url = os.getenv("BKAPP_FETCH_USER_INFO_URL")
         else:
-            self.token = FetchInstancePermission.build_auth(
-                settings.FETCH_INSTANCE_USERNAME, GlobalMetaConfig.get(FETCH_INSTANCE_TOKEN_KEY)
-            )
+            password = GlobalMetaConfig.get(FETCH_INSTANCE_TOKEN_KEY, default=uuid.uuid4().hex)
+            GlobalMetaConfig.set(FETCH_INSTANCE_TOKEN_KEY, password)
+            self.token = FetchInstancePermission.build_auth(settings.FETCH_INSTANCE_USERNAME, password)
             self.url = os.getenv("BKAPP_FETCH_INSTANCE_URL")
 
     def build_params(self):
