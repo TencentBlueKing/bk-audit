532af2cd78c15ceceff46a6fc0f087bf248b12d4
orenzhang <orenzhang@ovinc.cn>
2023-12-08 11:05:20 +0800

    perf: optimize for risk list

---
+++ src/backend/core/utils/page.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+TencentBlueKing is pleased to support the open source community by making
+蓝鲸智云 - 审计中心 (BlueKing - Audit Center) available.
+Copyright (C) 2023 THL A29 Limited,
+a Tencent company. All rights reserved.
+Licensed under the MIT License (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at http://opensource.org/licenses/MIT
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on
+an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+either express or implied. See the License for the
+specific language governing permissions and limitations under the License.
+We undertake not to change the open source license (MIT license) applicable
+to the current version of the project delivered to anyone in the future.
+"""
+
+from django.db.models import QuerySet
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.request import Request
+from rest_framework.settings import api_settings
+
+
+def paginate_queryset(queryset: QuerySet, request: Request) -> (QuerySet, PageNumberPagination):
+    """
+    分页 QuerySet
+    """
+
+    page: PageNumberPagination = api_settings.DEFAULT_PAGINATION_CLASS()
+    paged_queryset = page.paginate_queryset(queryset=queryset, request=request)
+    return queryset.model.objects.filter(pk__in=[item.pk for item in paged_queryset]), page
