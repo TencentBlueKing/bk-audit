d3a218bcaf673e0010105802b7adc5bfae1c050d
v_yyhoyang <nanasikeai@gmail.com>
2023-12-26 10:00:46 +0800

    feat: 操作日志审计切换为离线计算 #169 # Reviewed, transaction id: 934

--- src/frontend/src/views/strategy-manage/strategy-create/components/aiops/index.vue
+++ src/frontend/src/views/strategy-manage/strategy-create/components/aiops/index.vue
@@ -207,7 +207,7 @@
   const formData = ref<IFormData>({
     configs: {
       data_source: {
-        source_type: 'stream_source',
+        source_type: 'batch_join_source', // 默认使用离线模式：handleDataSourceType中不切换类型
       },
       config_type: 'EventLog',
       aiops_config: {
@@ -267,9 +267,9 @@
     fetchTable({
       table_type: item,
     });
-    formData.value.configs.data_source = {
-      source_type: sourceTypeMap.value[formData.value.configs.config_type],
-    };
+    // formData.value.configs.data_source = {
+    //   source_type: sourceTypeMap.value[formData.value.configs.config_type],
+    // };
     if (isInit) {
       emits('updateDataSource', formData.value.configs.data_source);
       emits('updateConfigType', formData.value.configs.config_type);
