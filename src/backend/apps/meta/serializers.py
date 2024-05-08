b5b6df515bcab4742064a82503c804905e0f185b
orenzhang <orenzhang@ovinc.cn>
2024-05-08 15:14:36 +0800

    feat: 修改标签为纯数字的文案 #323

--- src/backend/apps/meta/serializers.py
+++ src/backend/apps/meta/serializers.py
@@ -347,8 +347,10 @@ class SaveTagsRequestSerializer(serializers.Serializer):
     tag_name = serializers.CharField(label=gettext_lazy("Tag Name"))
 
     def validate_tag_name(self, tag_name: str) -> str:
-        if not TAG_NAME_REGEXP.match(tag_name) or tag_name.isdigit():
-            raise TagNameInValid(message="{} => {}".format(TagNameInValid.MESSAGE, tag_name))
+        if not TAG_NAME_REGEXP.match(tag_name):
+            raise TagNameInValid(message="{} {}".format(TagNameInValid.MESSAGE, tag_name))
+        if tag_name.isdigit():
+            raise TagNameInValid(message="{}".format(gettext("标签不能使用纯数字，请修改")))
         return tag_name
 
 
