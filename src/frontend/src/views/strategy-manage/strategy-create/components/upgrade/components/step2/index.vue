4115924d91cff3422c7d66cb736c0425e668f151
nanasikeai <nanasikeai@gmail.com>
2024-08-01 15:13:46 +0800

    fix: 策略 - 升级策略时携带的标签异常 --bug=128269797

--- src/frontend/src/views/strategy-manage/strategy-create/components/upgrade/components/step2/index.vue
+++ src/frontend/src/views/strategy-manage/strategy-create/components/upgrade/components/step2/index.vue
@@ -98,6 +98,7 @@
   const formRef = ref();
   const aiopsRef = ref();
   const formData = ref<Record<string, any>>({});
+  const strategyTagMap = ref<Record<string, string>>({});
   const rules = {
     'configs.data_source.system_id': [
       {
@@ -178,6 +179,17 @@
     ],
   };
 
+  // 获取标签列表
+  useRequest(StrategyManageService.fetchStrategyTags, {
+    defaultValue: [],
+    manual: true,
+    onSuccess(data) {
+      data.forEach((item) => {
+        strategyTagMap.value[item.tag_id] = item.tag_name;
+      });
+    },
+  });
+
   // 编辑状态获取数据
   const {
     loading: isEditDataLoading,
@@ -247,6 +259,10 @@
           ...params.configs.data_source,
           fields,
         };
+      }
+      if (params.tags) {
+        // eslint-disable-next-line max-len
+        params.tags = params.tags.map((item: string) => (strategyTagMap.value[item] ? strategyTagMap.value[item] : item));
       }
       InfoBox({
         title: t('升级确认'),
