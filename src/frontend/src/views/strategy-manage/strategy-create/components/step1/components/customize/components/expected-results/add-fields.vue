b6fac737901549acddd614b1bddcab6826a8872e
nanasikeai <nanasikeai@gmail.com>
2025-01-22 15:06:06 +0800

    fix: 预期结果的配置弹窗关闭方式 --bug=137395979

--- src/frontend/src/views/strategy-manage/strategy-create/components/step1/components/customize/components/expected-results/add-fields.vue
+++ src/frontend/src/views/strategy-manage/strategy-create/components/step1/components/customize/components/expected-results/add-fields.vue
@@ -1,16 +1,17 @@
 <template>
+  <div class="add-field-btn">
+    <audit-icon
+      type="add"
+      @click="handleShowPop" />
+  </div>
   <bk-popover
     ext-cls="field-custom-popover"
     height="300"
     :is-show="isShow"
     theme="light"
-    trigger="manual"
-    width="446">
-    <div class="add-field-btn">
-      <audit-icon
-        type="add"
-        @click="handleShowPop" />
-    </div>
+    trigger="click"
+    width="446"
+    @after-hidden="handleAfterHidden">
     <template #content>
       <div class="add-field-pop-content">
         <div
@@ -171,6 +172,10 @@
       aggregate: localAggregateList.value.find(item => !item.disabled)?.value, // 选择第一个可选项
     };
     formData.value.display_name =  `${field.display_name}_${formData.value.aggregate}`;
+  };
+
+  const handleAfterHidden = (value: { isShow: boolean}) => {
+    isShow.value = value.isShow;
   };
 
   const handleCancel = () => {
