2f05401aff4bf1636da91b7b7f0621f1806f8763
nanasikeai <nanasikeai@gmail.com>
2025-02-13 10:34:49 +0800

    fix: 详情不聚和显示错误 --bug=137984225

--- src/frontend/src/views/strategy-manage/list/components/risk-detection.vue
+++ src/frontend/src/views/strategy-manage/list/components/risk-detection.vue
@@ -250,7 +250,7 @@
 
   const getMetricName = (element: DatabaseTableFieldModel) => {
     const item = commonData.value.rule_audit_aggregate_type.find(item => item.value === element.aggregate);
-    return t(`[${item?.label}] ${element.display_name}`);
+    return t(`[${item?.label || t('不聚和')}] ${element.display_name}`);
   };
 
   const findLabelByValue = (data: Array<{
