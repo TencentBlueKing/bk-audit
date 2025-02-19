2837f645510e69b7f49b37b804419eb96367931e
nanasikeai <nanasikeai@gmail.com>
2025-02-19 19:43:19 +0800

    fix: 帮助文本前面的第一步等可以删除 --bug=138312131

--- src/frontend/src/views/strategy-manage/strategy-create/components/step1/components/customize/index.vue
+++ src/frontend/src/views/strategy-manage/strategy-create/components/step1/components/customize/index.vue
@@ -27,7 +27,7 @@
           required>
           <span
             v-bk-tooltips="{
-              content: t('第1步，此处配置的审计风险数据的数据源，用以后续步骤的处理。一般由“操作记录”与“资产数据”组合合成，但也可以自由组合而成。'),
+              content: t('审计规则的数据来源，联动后续步骤字段结构。如需组合数据，请提前创建并选择联表数据'),
               extCls:'strategy-config-type-tooltips'
             }"
             class="label-is-required"
@@ -73,7 +73,7 @@
           <template #label>
             <span
               v-bk-tooltips="{
-                content: t('第2步，可配置聚合规则。若无规则，则结果默认返回所有字段并不做聚合；若有规则，则返回配置字段。'),
+                content: t('需要哪些字段作为结果，每行记录可生成一个风险事件，展示在风险单内；也可用于第2步”单据展示“的字段映射；点击下方”预览“可提前预览风险单展示内容；'),
                 extCls:'strategy-config-type-tooltips'
               }"
               style="color: #63656e; cursor: pointer; border-bottom: 1px dashed #979ba5;">
@@ -94,7 +94,7 @@
           <template #label>
             <span
               v-bk-tooltips="{
-                content: t('第3步，完成数据处理后，配置对应的字段规则，筛选出我们期望的数据，可能是风险数据。'),
+                content: t('配置对应的字段与规则，筛选出我们期望的数据；可能是风险数据。'),
                 extCls:'strategy-config-type-tooltips'
               }"
               style="color: #63656e; cursor: pointer; border-bottom: 1px dashed #979ba5;">
