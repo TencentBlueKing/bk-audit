24e750d39cdc9e37f0bc2a60e6d0ba12a7bc3788
JetDrag <taptube@gmail.com>
2025-03-14 15:15:44 +0800

    feat: 系统诊断:系统详情页面增加系统诊断视图 --story=122229903
    1. 基于当前抽象整理Vison各组件代码，方便支持后续各类场景。
    2. Panel表添加场景支持，解耦系统图标ID和BKVision真实ID。
    3. 添加SystemID Action过滤支持。

--- src/backend/services/web/vision/models.py
+++ src/backend/services/web/vision/models.py
@@ -15,12 +15,20 @@ specific language governing permissions and limitations under the License.
 We undertake not to change the open source license (MIT license) applicable
 to the current version of the project delivered to anyone in the future.
 """
+import importlib
 
 from bk_audit.log.models import AuditInstance
 from django.db import models
+from django.db.models.enums import TextChoices
 from django.utils.translation import gettext_lazy
 
 from core.models import SoftDeleteModel
+from services.web.vision.exceptions import VisionHandlerInvalid
+
+
+class Scenario(TextChoices):
+    DEFAULT = "default", gettext_lazy("默认")
+    PER_APP = "per_app", gettext_lazy("按应用")
 
 
 class VisionPanel(SoftDeleteModel):
@@ -29,13 +37,22 @@ class VisionPanel(SoftDeleteModel):
     """
 
     id = models.CharField(gettext_lazy("ID"), primary_key=True, max_length=255)
+    vision_id = models.CharField(gettext_lazy("视图ID"), max_length=255, null=True)
     name = models.CharField(gettext_lazy("Name"), max_length=255)
     priority_index = models.IntegerField(gettext_lazy("优先指数"), default=0)
+    handler = models.CharField(gettext_lazy("处理器"), max_length=255, default="CommonVisionHandler")
+    scenario = models.CharField(gettext_lazy("场景"), max_length=255, default="default", choices=Scenario.choices)
 
     class Meta:
         verbose_name = gettext_lazy("仪表盘")
         verbose_name_plural = verbose_name
         ordering = ["-priority_index", "name"]
+
+    def get_vision_handler_class(self):
+        try:
+            return getattr(importlib.import_module("services.web.vision.handlers.query"), self.handler)
+        except (ImportError, AttributeError):
+            raise VisionHandlerInvalid()
 
 
 class VisionPanelInstance:
