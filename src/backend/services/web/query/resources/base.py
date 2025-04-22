646c461091913510e995ad90e1b27fbee233c7ab
JetDrag <taptube@gmail.com>
2025-04-22 21:44:12 +0800

    feat: 审计检索优化 --story=122841691
    1. 支持查询收藏

--- src/backend/services/web/query/resources/base.py
+++ src/backend/services/web/query/resources/base.py
@@ -19,13 +19,17 @@ to the current version of the project delivered to anyone in the future.
 import abc
 from typing import List
 
+from bk_resource.contrib.model import ModelResource
 from blueapps.utils.request_provider import get_request_username
+from django.utils.translation import gettext_lazy
 
 from apps.audit.resources import AuditMixinResource
 from apps.meta.models import SensitiveObject
 from apps.permission.handlers.actions import ActionEnum
 from apps.permission.handlers.permission import Permission
 from apps.permission.handlers.resource_types import ResourceEnum
+from services.web.query.models import FavoriteSearch
+from services.web.query.serializers import FavoriteSearchSerializer
 from services.web.query.utils.formatter import HitsFormatter
 
 
@@ -56,3 +60,37 @@ class SearchDataParser:
                 )
         # parse
         return [HitsFormatter(value, [*sensitive_objs, *private_sensitive_objs]).value for value in data]
+
+
+class CreateFavouriteSearchResource(ModelResource):
+    name = gettext_lazy("创建查询收藏")
+    lookup_field = "id"
+    model = FavoriteSearch
+    action = "list"
+    serializer_class = FavoriteSearchSerializer
+
+
+class UpdateFavouriteSearchResource(ModelResource):
+    name = gettext_lazy("更新查询收藏")
+    lookup_field = "id"
+    model = FavoriteSearch
+    action = "update"
+    serializer_class = FavoriteSearchSerializer
+
+
+class DeleteFavouriteSearchResource(ModelResource):
+    name = gettext_lazy("删除查询收藏")
+    lookup_field = "id"
+    model = FavoriteSearch
+    action = "destroy"
+    serializer_class = FavoriteSearchSerializer
+
+
+class ListFavouriteSearchResource(ModelResource):
+    """支持过滤查询收藏列表，支持的filter参数有：name、created_by"""
+
+    name = gettext_lazy("查询收藏列表")
+    model = FavoriteSearch
+    filter_fields = ["name", "created_by"]
+    action = "list"
+    serializer_class = FavoriteSearchSerializer
