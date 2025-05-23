641b40e682c5dda8d6ff57544770786df70272de
JetDrag <taptube@gmail.com>
2025-05-23 16:15:14 +0800

    feat: 审计检索优化 --story=122841691
    1.修正Serializer的to_internal_value处理。

--- src/backend/services/web/strategy_v2/serializers.py
+++ src/backend/services/web/strategy_v2/serializers.py
@@ -1048,7 +1048,7 @@ class RuleAuditWhereSerializer(serializers.Serializer):
         自定义序列化逻辑: 递归解析 conditions
         """
 
-        ret = super().to_representation(instance)
+        ret = super().to_internal_value(instance)
 
         # 递归处理 conditions
         conditions = instance.get('conditions', [])
