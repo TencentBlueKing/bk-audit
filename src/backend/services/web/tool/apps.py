e88e25cb9caba9543e02b4f57153caae0dd0261f
v_ljtoli <v_ljtoli@tencent.com>
2025-05-28 16:17:15 +0800

    feat: 工具1期-模型初始化 --story=124276449 # Reviewed, transaction id: 44255

---
+++ src/backend/services/web/tool/apps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from django.apps import AppConfig
+
+
+class ToolConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'services.web.tool'
