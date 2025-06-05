3e7232183de3858a9cf87513ed09afe3111820a9
nanasikeai <nanasikeai@gmail.com>
2025-06-05 15:05:47 +0800

    feat: 系统详情，tab新增流程 --story=122171245

---
+++ src/frontend/src/views/system-manage/detail/components/basic-info/index.vue
@@ -0,0 +1,78 @@
+<!--
+  TencentBlueKing is pleased to support the open source community by making
+  蓝鲸智云 - 审计中心 (BlueKing - Audit Center) available.
+  Copyright (C) 2023 THL A29 Limited,
+  a Tencent company. All rights reserved.
+  Licensed under the MIT License (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at http://opensource.org/licenses/MIT
+  Unless required by applicable law or agreed to in writing,
+  software distributed under the License is distributed on
+  an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+  either express or implied. See the License for the
+  specific language governing permissions and limitations under the License.
+  We undertake not to change the open source license (MIT license) applicable
+  to the current version of the project delivered to anyone in the future.
+-->
+<template>
+  <div class="system-basic-info">
+    <h3>{{ t('系统信息') }}</h3>
+    <div class="item">
+      <div class="item-title">
+        {{ t('应用ID') }}:
+      </div>
+      <div>{{ data.system_id }}</div>
+    </div>
+    <div class="item">
+      <div class="item-title">
+        {{ t('应用负责人') }}:
+      </div>
+      <div>
+        <edit-tag
+          :data="data.managers"
+          :max="5" />
+      </div>
+    </div>
+    <div class="item">
+      <div class="item-title">
+        {{ t('应用描述') }}:
+      </div>
+      <div class="description-value">
+        {{ data.description || '--' }}
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+  import { useI18n } from 'vue-i18n';
+
+  import SystemModel from '@model/meta/system';
+
+  import EditTag from '@components/edit-box/tag.vue';
+
+  interface Props {
+    data: SystemModel
+  }
+  defineProps<Props>();
+  const { t } = useI18n();
+</script>
+<style lang="postcss">
+.system-basic-info {
+  padding: 6px 14px;
+
+  .item {
+    display: flex;
+    margin: 30px 0;
+    font-size: 12px;
+    line-height: 26px;
+    color: #63656e;
+
+    .item-title {
+      width: 80px;
+      margin-right: 12px;
+      color: #979ba5;
+      text-align: right;
+    }
+  }
+}
+</style>
