c43cd94aee71c0282d5b113fb2d299c33390081c
raja <1647193241@qq.com>
2025-06-06 12:16:59 +0800

    feat: 系统接入提效2期：V1闭环系统与权限模型接入 --story=121659470
    1. 新增操作增删改接口，新增系统创建，编辑接口
    2. 系统列表增加筛选条件和返回信息
    3. 分支合并

--- src/backend/core/utils/tools.py
+++ src/backend/core/utils/tools.py
@@ -15,7 +15,6 @@ specific language governing permissions and limitations under the License.
 We undertake not to change the open source license (MIT license) applicable
 to the current version of the project delivered to anyone in the future.
 """
-import uuid
 
 from blueapps.utils.request_provider import get_local_request
 from django.conf import settings
@@ -47,8 +46,3 @@ def is_product() -> bool:
     """
 
     return settings.RUN_MODE == "PRODUCT"
-
-
-def unique_id():
-    """生成32个字符的唯一ID ."""
-    return uuid.uuid3(uuid.uuid1(), uuid.uuid4().hex).hex
