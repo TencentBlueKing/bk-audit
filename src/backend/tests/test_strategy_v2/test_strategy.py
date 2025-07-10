58684aed6222e1be74b98c86a4564a8a29c1b4ea
v-ljt <A2186800569@163.com>
2025-07-10 14:19:22 +0800

    feat: 工具1期 --story=123453683 1.修改列表接口分页参数,条件交互 2,工具关联策略数据格式问题 # Reviewed, transaction id: 50153

--- src/backend/tests/test_strategy_v2/test_strategy.py
+++ src/backend/tests/test_strategy_v2/test_strategy.py
@@ -73,7 +73,10 @@ class StrategyTest(TestCase):
                 "field_name": field_name,
                 "display_name": "字段名",
                 "is_priority": True,
-                "drill_config": [{"tool": {"uid": "fake_tool_uid", "version": 1, "params": {}}, "config": []}],
+                "drill_config": {
+                    "tool": {"uid": "fake_tool_uid", "version": 1, "params": {}},
+                    "config": [],
+                },
             }
         ]
 
