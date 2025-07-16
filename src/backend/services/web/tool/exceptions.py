881662ab71d53f9bc222859ab16891f5d4833a83
raja <1647193241@qq.com>
2025-07-16 17:53:02 +0800

    feat: 工具1期：工具使用-输入类型转换 --story=125773510
    1. 增加变量值解析器，支持多种输入类型的解析。
    2. 新增异常处理类，处理输入变量缺失、格式错误和结构错误等情况。
    3. 更新 SQL 数据查询执行器，增强变量渲染功能，确保输入变量的有效性。
    4. 增加相关单元测试，验证新功能的正确性。
    5. 更新SqlQueryAnalysis以使用RangeVariableData处理范围参数

--- src/backend/services/web/tool/exceptions.py
+++ src/backend/services/web/tool/exceptions.py
@@ -14,9 +14,12 @@ specific language governing permissions and limitations under the License.
 We undertake not to change the open source license (MIT license) applicable
 to the current version of the project delivered to anyone in the future.
 """
+from typing import Any
+
 from django.utils.translation.trans_null import gettext_lazy
 
 from apps.exceptions import CoreException
+from services.web.tool.constants import FieldCategory
 
 
 class ToolException(CoreException):
@@ -25,22 +28,101 @@ class ToolException(CoreException):
 
 class DataSearchSimpleModeNotSupportedError(ToolException):
     MESSAGE = gettext_lazy("当前暂不支持 '简易模式'(simple) 的数据查询工具")
+    STATUS_CODE = 400
     ERROR_CODE = "001"
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
 
-class ToolConfigException(ToolException):
-    MESSAGE = gettext_lazy("Tool config error")
-    ERROR_CODE = "002"
-
-
 class DataSearchTablePermission(ToolException):
-    STATUS_CODE = 403
+    STATUS_CODE = 409
     ERROR_CODE = "003"
     MESSAGE = gettext_lazy("用户{user}没有权限访问数据源{data_source}")
 
     def __init__(self, user, data_source, *args, **kwargs):
         self.MESSAGE = self.MESSAGE.format(user=user, data_source=data_source)
+        super().__init__(*args, **kwargs)
+
+
+class InputVariableMissingError(ToolException):
+    """
+    输入变量缺失
+    """
+
+    STATUS_CODE = 400
+    ERROR_CODE = "004"
+    MESSAGE = gettext_lazy("输入变量“{var_name}”必填")
+
+    def __init__(self, var_name: str, *args, **kwargs):
+        self.MESSAGE = self.MESSAGE.format(var_name=var_name)
+        super().__init__(*args, **kwargs)
+
+
+class InputVariableValueError(ToolException):
+    """
+    用户提供的变量值错误的基类异常。
+    """
+
+    STATUS_CODE = 400
+    ERROR_CODE = "005"
+    MESSAGE = gettext_lazy("用户输入变量值无效")
+
+
+class InvalidVariableFormatError(InputVariableValueError):
+    """
+    当变量值的格式不正确时抛出。
+    (例如，无效的日期字符串，非数字的值)
+    """
+
+    ERROR_CODE = "006"
+    MESSAGE = gettext_lazy("变量类型“{var_type}”的值“{value}”格式无效")
+
+    def __init__(self, var_type: FieldCategory, value: Any, *args, **kwargs):
+        self.MESSAGE = self.MESSAGE.format(var_type=var_type.label, value=value)
+        super().__init__(*args, **kwargs)
+
+
+class InvalidVariableStructureError(InputVariableValueError):
+    """
+    当变量值的结构不正确时抛出。
+    (例如，期望一个列表，但收到了一个字符串)
+    """
+
+    ERROR_CODE = "007"
+    MESSAGE = gettext_lazy("变量类型“{var_type}”的值结构无效: 期望“{expected_structure}”,但收到的是“{actual_type}”")
+
+    def __init__(self, var_type: FieldCategory, expected_structure: str, value: Any, *args, **kwargs):
+        actual_type = type(value).__name__
+        self.MESSAGE = self.MESSAGE.format(
+            var_type=var_type.label, expected_structure=expected_structure, actual_type=actual_type
+        )
+        super().__init__(*args, *kwargs)
+
+
+class VariableHasNoParseFunction(ToolException):
+    """
+    变量没有解析函数
+    """
+
+    STATUS_CODE = 500
+    ERROR_CODE = "008"
+    MESSAGE = gettext_lazy("变量类型“{var_type}”没有解析函数")
+
+    def __init__(self, var_type: FieldCategory, *args, **kwargs):
+        self.MESSAGE = self.MESSAGE.format(var_type=var_type.label)
+        super().__init__(*args, **kwargs)
+
+
+class ParseVariableError(ToolException):
+    """
+    解析变量异常
+    """
+
+    STATUS_CODE = 500
+    ERROR_CODE = "009"
+    MESSAGE = gettext_lazy("解析变量类型“{var_type}”的值“{value}”异常,请联系系统管理员")
+
+    def __init__(self, var_type: FieldCategory, value: str, *args, **kwargs):
+        self.MESSAGE = self.MESSAGE.format(var_type=var_type.label, value=value)
         super().__init__(*args, **kwargs)
