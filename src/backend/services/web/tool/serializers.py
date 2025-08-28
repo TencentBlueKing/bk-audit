fa9d41994c5d09d04cc0a1ad4ad9ba4ca943e7b1
v-ljt <A2186800569@163.com>
2025-08-28 17:01:49 +0800

    feat: 工具1期 --story=123453683 1.修改list工具接口返回字段 2.修复编辑工具时标签丢失 3.标签组优化 # Reviewed, transaction id: 56138
    (cherry picked from commit fc99c825b77338d0eea3481a748915decb66b08b)

--- src/backend/services/web/tool/serializers.py
+++ src/backend/services/web/tool/serializers.py
@@ -146,6 +146,8 @@ class ToolListResponseSerializer(serializers.ModelSerializer):
             "version",
             "description",
             "namespace",
+            "updated_by",
+            "updated_at",
             "created_by",
             "created_at",
             "tags",
