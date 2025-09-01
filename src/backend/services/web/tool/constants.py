c2ccd9c3bfeb158dd0557f7f3d30a1ad8537698e
v-ljt <a2186800569@163.com>
2025-09-01 17:13:41 +0800

    feat: 字段下钻支持关联多个工具 --story=126594081 # Reviewed, transaction id: 57869
    (cherry picked from commit 6f6ee86131c632b4e9321aca02dd0424d4b6acea)
    
    (cherry picked from commit 54bc9ba0625cad4fb3b36a1025abed272042917b)
    Signed-off-by: raja <1647193241@qq.com>
    (cherry picked from commit 0916381f66d6d5113e1b18aebf3edb4cee7bf457)

--- src/backend/services/web/tool/constants.py
+++ src/backend/services/web/tool/constants.py
@@ -194,6 +194,7 @@ class DrillConfig(BaseModel):
 
     tool: Tool
     config: List[ToolDrillConfig] = PydanticField(default_factory=list)
+    drill_name: str = PydanticField("", description="下钻工具名称")
 
 
 class SQLDataSearchOutputField(DataSearchBaseField):
@@ -201,7 +202,7 @@ class SQLDataSearchOutputField(DataSearchBaseField):
     SQL数据查询输出字段
     """
 
-    drill_config: Optional[DrillConfig] = None  # 下钻配置
+    drill_config: Optional[List[DrillConfig]] = None  # 下钻配置
     enum_mappings: Optional[EnumMappingConfig] = None
 
 
