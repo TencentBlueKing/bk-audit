e638cb1c1e108a3cb306287f745e3ec4a2491a28
v_zzlgzhong <v_zzlgzhong@tencent.com>
2025-11-03 17:23:59 +0800

    feat: 审计风险工单处理说明新增富文本 --story=127758332

---
+++ src/frontend/src/domain/source/upload-manage.ts
@@ -0,0 +1,29 @@
+/*
+  TencentBlueKing is pleased to support the open source community by making
+  蓝鲸智云 - 审计中心 (BlueKing - Audit Center) available.
+  Copyright (C) 2023 THL A29 Limited,
+  a Tencent company. All rights reserved.
+  Licensed under the MIT License (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at http://opensource.org/licenses/MIT
+  Unless required by applicable law or agreed to in writing,
+  software distributed under the License is distributed on
+  an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+  either express or implied. See the License for the
+  specific language governing permissions and limitations under the License.
+  We undertake not to change the open source license (MIT license) applicable
+  to the current version of the project delivered to anyone in the future.
+*/
+import ModuleBase from './module-base';
+
+import Request from '@/utils/request';
+
+class UpLoadManage extends ModuleBase {
+  UploadNewImage(params: any) {
+    return Request.post('/api/v1/blob_storage/upload/', {
+      params,
+    });
+  }
+}
+export default new UpLoadManage();
+
