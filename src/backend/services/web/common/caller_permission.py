914fe97f5879adb6596784835d9538117323cf9b
JetDrag <taptube@gmail.com>
2025-11-06 21:45:57 +0800

    feat: 新增关联事件字段搜索功能 --story=126826559
    1. 支持在列表中传递的单个简单类型变量校验。

--- src/backend/services/web/common/caller_permission.py
+++ src/backend/services/web/common/caller_permission.py
@@ -261,7 +261,9 @@ def validate_tool_variables_with_risk(
                     expected = event_record.get("event_data", {}).get(rule["target"])
                 value_to_match = collection_to_tuple(value)
                 single_value = value_to_match[0] if len(value_to_match) == 1 else None
-                if not expected or (value_to_match not in expected) or (single_value and single_value not in expected):
+                if not expected or (
+                    (value_to_match not in expected) and (single_value and single_value not in expected)
+                ):
                     match_result.append(False)
                     continue
             match_result.append(True)
