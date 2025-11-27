e4159270b02dd89cdfdb0775a093eaeb5bdb3c3a
JetDrag <taptube@gmail.com>
2025-11-27 17:11:42 +0800

    feat: 新增手动创建风险单与关联事件 -- 后台 --story=128746300
    1. 附带字段类型信息。
    2. 优化权限体验。
    3. 当前仅支持规则审计。

--- src/backend/services/web/risk/handlers/risk.py
+++ src/backend/services/web/risk/handlers/risk.py
@@ -219,11 +219,11 @@ class RiskHandler:
 
         # 存在则更新结束时间
         if risk:
-            last_end_time = int(event["event_time"] / 1000)
+            last_end_time = event["event_time"] / 1000
             logger.info("[UpdateRisk] Risk exists. risk_id=%s; last_end_time=%s", risk.risk_id, last_end_time)
             # 只在事件的时间更新的时候存储
-            if int(risk.event_end_time.timestamp()) < last_end_time:
-                risk.event_end_time = datetime.datetime.fromtimestamp(event["event_time"] / 1000)
+            if risk.event_end_time.timestamp() < last_end_time:
+                risk.event_end_time = datetime.datetime.fromtimestamp(last_end_time)
                 risk.save(update_fields=["event_end_time"])
             return False, risk
 
