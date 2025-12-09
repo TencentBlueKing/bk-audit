1325ee2d494e6bbea64ac622e232b771a092648a
v_zzlgzhong <zzl15727777247@163.com>
2025-12-09 16:09:17 +0800

    feat: 工具支持API接口-前端页面 --story=128849986

---
+++ src/frontend/src/views/tools/tools-square/add/components/api/params-config.vue
@@ -0,0 +1,620 @@
+<!-- eslint-disable vue/no-template-shadow -->
+<!--
+  TencentBlueKing is pleased to support the open source community by making
+  蓝鲸智云 - 审计中心 (BlueKing - Audit Center) available.
+  Copyright (C) 2023 THL A29 Limited,
+  a Tencent company. All rights reserved.
+  Licensed under the MIT License (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at http://opensource.org/licenses/MIT
+  Unless required by applicable law or agreed to in writing,
+  software distributed under the License is distributed on
+  an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+  either express or implied. See the License for the
+  specific language governing permissions and limitations under the License.
+  We undertake not to change the open source license (MIT license) applicable
+  to the current version of the project delivered to anyone in the future.
+-->
+<template>
+  <bk-form-item
+    label-width="160">
+    <div class="render-field ">
+      <div class="field-header-row">
+        <div
+          class="field-value is-required"
+          style="flex: 0 0 150px;">
+          {{ t('参数名') }}
+        </div>
+        <div
+          class="field-value"
+          style="flex: 0 0 100px;">
+          {{ t('显示名') }}
+        </div>
+        <div
+          class="field-value"
+          style="flex: 0 0 150px;">
+          {{ t('传参方式') }}
+        </div>
+        <div
+          class="field-value">
+          {{ t('参数说明') }}
+        </div>
+        <div
+          class="field-value"
+          style="flex: 0 0 200px;">
+          {{ t('是否必填') }}
+          <bk-popover
+            ref="requiredListRef"
+            allow-html
+            content="#hidden_pop_content"
+            ext-cls="field-required-pop"
+            placement="top"
+            theme="light"
+            trigger="click"
+            width="100">
+            <audit-icon
+              style="margin-left: 4px; font-size: 16px;color: #3a84ff; cursor: pointer;"
+              type="piliangbianji" />
+          </bk-popover>
+          <div style="display: none">
+            <div id="hidden_pop_content">
+              <div
+                v-for="(item, index) in requiredList"
+                :key="index"
+                class="field-required-item"
+                @click="handleRequiredClick(item)">
+                {{ item.label }}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          class="field-value is-required"
+          style="flex: 0 0 120px;">
+          {{ t('前端类型') }}
+        </div>
+        <div
+          class="field-value"
+          style="flex: 0 0 250px;">
+          {{ t('默认值') }}
+        </div>
+        <div
+          class="field-value"
+          style="flex: 0 0 120px;">
+          {{ t('是否可见') }}
+          <bk-popover
+            ref="requiredListRef"
+            allow-html
+            content="#hidden_pop_content2"
+            ext-cls="field-required-pop"
+            placement="top"
+            theme="light"
+            trigger="click"
+            width="100">
+            <audit-icon
+              style="margin-left: 4px; font-size: 16px;color: #3a84ff; cursor: pointer;"
+              type="piliangbianji" />
+          </bk-popover>
+          <div style="display: none">
+            <div id="hidden_pop_content2">
+              <div
+                v-for="(item, index) in isViewsList"
+                :key="index"
+                class="field-required-item"
+                @click="handleViewsClick(item)">
+                {{ item.label }}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          class="field-value"
+          style="flex: 0 0 80px;" />
+      </div>
+    </div>
+    <audit-form
+      ref="tableInputFormRef"
+      form-type="vertical"
+      :model="formData"
+      :rules="rules">
+      <template
+        v-for="(item, index) in paramList"
+        :key="item.id">
+        <div class="field-row">
+          <div
+            class="field-value"
+            style="flex: 0 0 150px;">
+            <bk-form-item
+              error-display-type="tooltips"
+              label=""
+              label-width="0">
+              <bk-input
+                v-if="item.field_category !== 'time_range_select'"
+                v-model="item.raw_name" />
+              <div v-else>
+                <div style="display: flex; border-bottom: 1px solid  #dcdee5;">
+                  <bk-input
+                    v-model="item.split_config.start_field" />
+                  <span>
+                    <bk-tag theme="info">
+                      {{ t('开始时间') }}
+                    </bk-tag>
+                  </span>
+                </div>
+                <div style="display: flex;">
+                  <bk-input
+                    v-model="item.split_config.end_field" />
+                  <span>
+                    <bk-tag theme="warning">
+                      {{ t('结束时间') }}
+                    </bk-tag>
+                  </span>
+                </div>
+              </div>
+            </bk-form-item>
+          </div>
+          <!-- 显示名 -->
+          <div
+            class="field-value"
+            style="flex: 0 0 100px;">
+            <bk-form-item
+              error-display-type="tooltips"
+              label=""
+              label-width="0">
+              <bk-input
+                v-model="item.display_name" />
+            </bk-form-item>
+          </div>
+          <!-- 传参方式 -->
+          <div
+            class="field-value"
+            style="flex: 0 0 150px;">
+            <bk-form-item
+              error-display-type="tooltips"
+              label=""
+              label-width="0">
+              <bk-select
+                v-model="item.position"
+                class="bk-select"
+                size="small">
+                <bk-option
+                  v-for="(paramsTypeItem, paramsTypeIndex) in paramsTypeList"
+                  :id="paramsTypeItem.value"
+                  :key="paramsTypeIndex"
+                  :name="paramsTypeItem.label" />
+              </bk-select>
+            </bk-form-item>
+          </div>
+          <!-- 参数说明 -->
+          <div
+            class="field-value">
+            <bk-form-item
+              error-display-type="tooltips"
+              label=""
+              label-width="0">
+              <bk-input
+                v-model="item.description" />
+            </bk-form-item>
+          </div>
+          <!-- 是否必填 -->
+          <div
+            class="field-value"
+            style="flex: 0 0 200px;">
+            <bk-form-item
+              error-display-type="tooltips"
+              label=""
+              label-width="0">
+              <bk-select
+                v-model="item.required"
+                class="bk-select"
+                size="small">
+                <bk-option
+                  v-for="(requiredItem, requiredIndex) in requiredList"
+                  :id="requiredItem.value"
+                  :key="requiredIndex.id"
+                  :name="requiredItem.label" />
+              </bk-select>
+            </bk-form-item>
+          </div>
+          <!-- 前端类型 -->
+          <div
+            class="field-value"
+            style="flex: 0 0 120px;">
+            <bk-form-item
+              error-display-type="tooltips"
+              label=""
+              label-width="0">
+              <bk-select
+                v-model="item.field_category"
+                class="bk-select"
+                :clearable="false"
+                size="small">
+                <bk-option
+                  v-for="(frontendTypeItem, frontendTypeItemIndex) in frontendTypeList"
+                  :id="frontendTypeItem.value"
+                  :key="frontendTypeItemIndex"
+                  :name="frontendTypeItem.label" />
+              </bk-select>
+            </bk-form-item>
+          </div>
+          <!-- 默认值 -->
+          <div
+            class="field-value"
+            style="flex: 0 0 250px;">
+            <bk-form-item
+              error-display-type="tooltips"
+              label=""
+              label-width="0">
+              <bk-input
+                v-if="item.field_category == 'number_input'"
+                v-model="item.default_value"
+                clearable
+                type="number" />
+              <audit-user-selector
+                v-else-if="item.field_category === 'person_select'"
+                v-model="item.default_value" />
+              <date-picker
+                v-else-if="item.field_category === 'time_range_select' || item.field_category === 'time-ranger'"
+                v-model="item.time_range"
+                style="width: 100%" />
+
+              <bk-date-picker
+                v-else-if="item.field_category === 'time_select' || item.field_category === 'time-picker'"
+                v-model="item.default_value"
+                append-to-body
+                clearable
+                style="width: 100%"
+                type="datetime" />
+              <bk-input
+                v-else
+                v-model="item.default_value"
+                clearable />
+            </bk-form-item>
+          </div>
+          <!-- 是否可见 -->
+          <div
+            class="field-value"
+            style="flex: 0 0 120px;">
+            <bk-form-item
+              error-display-type="tooltips"
+              label=""
+              label-width="0">
+              <bk-select
+                v-model="item.is_show"
+                class="bk-select"
+                size="small">
+                <bk-option
+                  v-for="(isViewItem, isViewItemIndex) in isViewsList"
+                  :id="isViewItem.value"
+                  :key="isViewItemIndex"
+                  :name="isViewItem.label" />
+              </bk-select>
+            </bk-form-item>
+          </div>
+          <!-- 按钮 -->
+          <div
+            class="field-value"
+            style="flex: 0 0 80px;">
+            <bk-form-item
+              error-display-type="tooltips"
+              label=""
+              label-width="0">
+              <audit-icon
+                class="add-fill field-icon"
+                type="add-fill"
+                @click="handelAddItem" />
+              <audit-icon
+                v-if="paramList.length > 1"
+                class="reduce-fill field-icon"
+                type="reduce-fill"
+                @click="handelDeleteItem(index)" />
+            </bk-form-item>
+          </div>
+        </div>
+      </template>
+    </audit-form>
+  </bk-form-item>
+</template>
+<script setup lang='tsx'>
+  import { ref, watch } from 'vue';
+  import { useI18n } from 'vue-i18n';
+
+  interface Props {
+    inputVariable: Array<Record, any>;
+  }
+
+  interface Exposes {
+    getData: () => void;
+  }
+
+  const props = defineProps<Props>();
+  console.log('props', props.inputVariable);
+
+  const { t } = useI18n();
+  const formData = ref();
+  const rules = ref();
+  const paramList = ref([
+    {
+      is_show: 'true',
+      position: '',
+      raw_name: '',
+      required: 'true',
+      description: '',
+      display_name: '',
+      split_config: {
+        end_field: '',
+        start_field: '',
+      },
+      default_value: '',
+      time_range: [],
+      field_category: 'input',
+    },
+  ]);
+  const requiredList = ref([{
+    id: '1',
+    value: 'true',
+    label: t('是'),
+  }, {
+    id: '2',
+    value: 'false',
+    label: t('否'),
+  }]);
+  const paramsTypeList = ref([{
+    value: 'query',
+    label: 'query',
+  }, {
+    value: 'body',
+    label: 'body',
+  }]);
+  const tableInputFormRef = ref();
+  const frontendTypeList = ref([{
+    value: 'input',
+    label: '输入框',
+  }, {
+    value: 'number_input',
+    label: '数字输入框',
+  }, {
+    value: 'person_select',
+    label: '人员选择器',
+  }, {
+    value: 'time_range_select',
+    label: '时间范围选择器',
+  }, {
+    value: 'time_select',
+    label: '时间选择器',
+  },
+  ]);
+
+  const isViewsList = ref([{
+    id: '1',
+    value: 'true',
+    label: t('可见'),
+  }, {
+    id: '2',
+    value: 'false',
+    label: t('不可见'),
+  }]);
+  // 添加一项
+  const handelAddItem = () => {
+    paramList.value.push({
+      is_show: 'true',
+      position: '',
+      raw_name: '',
+      required: 'true',
+      description: '',
+      display_name: '',
+      split_config: {
+        end_field: '',
+        start_field: '',
+      },
+      time_range: [],
+      default_value: null,
+      field_category: '',
+    });
+  };
+  // 删除一项
+  const handelDeleteItem = (index: number) => {
+    paramList.value.splice(index, 1);
+  };
+
+  const handleRequiredClick = (item: Record<string, any>) => {
+    paramList.value = paramList.value.map((listItem: Record<string, any>) => {
+      // eslint-disable-next-line no-param-reassign
+      listItem.required = item.value;
+      return listItem;
+    });
+  };
+  const handleViewsClick = (item: Record<string, any>) => {
+    console.log('handleViewsClick', item);
+    paramList.value = paramList.value.map((listItem: Record<string, any>) => {
+      // eslint-disable-next-line no-param-reassign
+      listItem.is_show = item.value;
+      return listItem;
+    });
+  };
+
+  watch(
+    () => props.inputVariable,
+    (val) => {
+      if (val && val.length > 0) {
+        console.log(val);
+        // paramList.value = val.map((item: Record<string, any>) => {
+        //   console.log('item>>');
+        //   return {
+        //     ...item,
+        //     time_range: [],
+        //   };
+        // });
+      }
+    },
+    {
+      deep: true,
+      immediate: true,
+    },
+  );
+
+  defineExpose<Exposes>({
+    getData() {
+      return paramList.value;
+    },
+  });
+
+</script>
+
+<style lang="postcss" scoped>
+.render-field {
+  display: flex;
+  min-width: 640px;
+  overflow: hidden;
+  border: 1px solid #dcdee5;
+  border-radius: 2px;
+  user-select: none;
+  flex-direction: column;
+  flex: 1;
+
+  .field-select {
+    width: 40px;
+    text-align: center;
+    background: #fafbfd;
+  }
+
+  .field-operation {
+    width: 170px;
+    padding-left: 16px;
+    background: #fafbfd;
+    border-left: 1px solid #dcdee5;
+  }
+
+
+}
+
+:deep(.field-value) {
+  display: flex;
+  flex: 1;
+
+  /* width: 180px; */
+  overflow: hidden;
+  border-left: 1px solid #dcdee5;
+  align-items: center;
+
+  .field-value-div {
+    display: flex;
+    padding: 0 8px;
+    cursor: pointer;
+    align-items: center;
+
+    &:hover {
+      .remove-btn {
+        display: block;
+      }
+    }
+
+    .remove-btn {
+      position: absolute;
+      top: 36%;
+      right: 28px;
+      z-index: 1;
+      display: none;
+      font-size: 12px;
+      color: #c4c6cc;
+      transition: all .15s;
+
+      &:hover {
+        color: #979ba5;
+      }
+
+      &.is-popconfirm-visible {
+        display: block;
+      }
+    }
+
+    .remove-mappings-btn {
+      top: 40%;
+      right: 8px;
+    }
+
+    .renew-tips {
+      position: absolute;
+      right: 8px;
+      font-size: 14px;
+      color: #3a84ff;
+    }
+  }
+
+  .bk-form-item.is-error {
+    .bk-input--text {
+      background-color: #ffebeb;
+    }
+  }
+
+  .bk-form-item {
+    width: 100%;
+    margin-bottom: 0;
+
+    .bk-input,
+    .bk-date-picker-editor,
+    .bk-select-trigger,
+    .bk-select-tag,
+    .date-picker {
+      height: 42px !important;
+      border: none;
+    }
+
+    .icon-wrapper {
+      top: 6px;
+    }
+
+    .bk-input.is-focused:not(.is-readonly) {
+      border: 1px solid #3a84ff;
+      outline: 0;
+      box-shadow: 0 0 3px #a3c5fd;
+    }
+
+    .bk-form-error-tips {
+      top: 12px
+    }
+  }
+}
+
+
+.field-header-row {
+  display: flex;
+  height: 42px;
+  font-size: 12px;
+  line-height: 40px;
+  color: #313238;
+  background: #f0f1f5;
+
+  .field-value {
+    padding-left: 8px;
+  }
+
+  .field-value.is-required {
+    &::after {
+      margin-left: 4px;
+      color: red;
+      content: '*';
+    }
+  }
+
+  .field-select,
+  .field-operation {
+    background: #f0f1f5;
+  }
+}
+
+.field-row {
+  display: flex;
+  overflow: hidden;
+  font-size: 12px;
+  line-height: 42px;
+  color: #63656e;
+  border-right: 1px solid #dcdee5;
+  border-bottom: 1px solid #dcdee5;
+}
+
+.field-icon {
+  margin-left: 20px;
+  color: #c4c6cc;
+}
+</style>
---
+++ src/frontend/src/views/tools/tools-square/add/components/api/result/index.vue
@@ -0,0 +1,277 @@
+<!--
+  TencentBlueKing is pleased to support the open source community by making
+  蓝鲸智云 - 审计中心 (BlueKing - Audit Center) available.
+  Copyright (C) 2023 THL A29 Limited,
+  a Tencent company. All rights reserved.
+  Licensed under the MIT License (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at http://opensource.org/licenses/MIT
+  Unless required by applicable law or agreed to in writing,
+  software distributed under the License is distributed on
+  an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+  either express or implied. See the License for the
+  specific language governing permissions and limitations under the License.
+  We undertake not to change the open source license (MIT license) applicable
+  to the current version of the project delivered to anyone in the future.
+-->
+<template>
+  <card-part-vue :title="t('查询结果设置')">
+    <template #content>
+      <div class="group-box">
+        <span>{{ t('是否分组') }}</span>
+        <bk-switcher
+          v-model="outputConfigEnableGrouping.enable_grouping"
+          class="group"
+          theme="primary" />
+        <span v-if="!outputConfigEnableGrouping.enable_grouping">
+          <audit-icon
+            class="info-fill"
+            type="info-fill" />
+          <span class="info-fill-tex">{{ t('开启后，查询结果将按字段分组展示') }}</span>
+        </span>
+        <span v-else>
+          <span
+            class="group-button"
+            @click="handleAddGroup">
+            <audit-icon
+              class="plus-circle"
+              type="plus-circle" />
+            <span class="plus-circle-tex">{{ t('添加分组') }}</span>
+          </span>
+          <span class="line" />
+          <span>
+            <span
+              class="plus-circle-tex"
+              @click="handleOpenGroup">{{ t(openGroup ? '一键展开分组' : '一键收起分组') }}</span>
+          </span>
+        </span>
+      </div>
+      <content
+        v-if="!outputConfigEnableGrouping.enable_grouping"
+        ref="contentRef"
+        :output-config-groups="outputConfig.groups"
+        :resultData="resultData" />
+      <group-content
+        v-else
+        ref="groupContentRef"
+        :output-config-groups="outputConfig.groups"
+        :result-data="resultData" />
+    </template>
+  </card-part-vue>
+</template>
+<script setup lang='tsx'>
+  import { ref } from 'vue';
+  import { useI18n } from 'vue-i18n';
+
+  import CardPartVue from '../../card-part.vue';
+
+  import content from './content.vue';
+  import groupContent from './group-content.vue';
+
+
+  interface Props {
+    resultData: any,
+    outputConfig: {
+      enable_grouping: boolean,
+      groups: Array<Record, any>,
+    },
+  }
+  interface Exposes {
+    handleGetResultConfig: () => Config;
+  }
+
+  const props = defineProps<Props>();
+  const contentRef = ref();
+  const { t } = useI18n();
+  const groupContentRef = ref();
+  const openGroup = ref(false);
+  const outputConfigEnableGrouping = ref({
+    enable_grouping: !false,
+  });
+  // 添加分组
+  const handleAddGroup = () => {
+    console.log('添加分组');
+    groupContentRef.value?.addGroup();
+  };
+  // 一键展开分组
+  const handleOpenGroup = () => {
+    openGroup.value = !openGroup.value;
+    groupContentRef.value?.openGroup(openGroup.value);
+  };
+  console.log('propsresultData>>', props.resultData);
+  // 获取配置信息
+  const getResultConfig = () => {
+    console.log('提交获取配置信息>>index');
+    if (outputConfigEnableGrouping.value.enable_grouping) {
+      const groupContentRefData =  groupContentRef.value?.handleGetResultConfig();
+      console.log('groupContentRefData>>', groupContentRefData);
+      const isGroupOutputConfig = {
+        enable_grouping: true,
+        groups: [],
+      };
+      isGroupOutputConfig.groups = groupContentRefData.map((item) => {
+        const groupConfig = {
+          name: item.name, // 分组名
+          output_fields: item.config?.map((configItem: any) => {
+            // 表格
+            if (configItem.type === 'table') {
+              return {
+                raw_name: configItem.name,
+                json_path: configItem.json_path,
+                description: configItem.listDescription || '',
+                display_name: configItem.listName || '',
+                drill_config: null,
+                enum_mappings: null,
+                field_config: {
+                  field_type: 'table',
+                  output_fields: configItem.list.map((listItem: any) => ({
+                    raw_name: listItem.name,
+                    json_path: listItem.json_path,
+                    description: listItem?.description || '',
+                    display_name: listItem?.display_name || '',
+                    drill_config: listItem?.drill_config || null,
+                    enum_mappings: {
+                      mappings: listItem?.enum_mappings.mappings || [],
+                    },
+                  })),
+                },
+              };
+            }
+            // 对象
+            return {
+              raw_name: configItem.name,
+              json_path: configItem.json_path,
+              description: configItem.config?.description || '',
+              display_name: configItem.config?.display_name || '',
+              drill_config: configItem.config?.drill_config || null,
+              field_config: {
+                field_type: 'kv',
+              },
+              enum_mappings: {
+                mappings: configItem.config?.mappings || [],
+              },
+            };
+          }),
+        };
+        console.log('groupConfig', groupConfig);
+        return groupConfig;
+      });
+      console.log('isGroupOutputConfig', isGroupOutputConfig);
+      return isGroupOutputConfig;
+    }
+    const contentRefData  = contentRef.value?.handleGetResultConfig();
+    const noGroupOutputConfig = {
+      enable_grouping: false,
+      groups: [],
+    };
+    console.log('contentRefData', contentRefData);
+
+    noGroupOutputConfig.groups[0] = {
+      name: '',
+      output_fields: contentRefData.map((item: any) => {
+        if (item.type === 'table') {
+          return {
+            raw_name: item.name,
+            json_path: item.json_path,
+            description: item.listDescription || '',
+            display_name: item.listName || '',
+            drill_config: null,
+            enum_mappings: null,
+            field_config: {
+              field_type: 'table',
+              output_fields: item.list.map((listItem: any) => ({
+                raw_name: listItem.name,
+                json_path: listItem.json_path,
+                description: listItem?.description || '',
+                display_name: listItem?.display_name || '',
+                drill_config: listItem?.drill_config || null,
+                enum_mappings: {
+                  mappings: listItem?.enum_mappings.mappings || [],
+                },
+              })),
+            },
+          };
+        }
+        console.log('对象');
+
+        return {
+          raw_name: item.name,
+          json_path: item.json_path,
+          description: item?.config.description || '',
+          display_name: item?.config.display_name || '',
+          drill_config: item?.config.drill_config || null,
+          enum_mappings: {
+            mappings: item?.config.mappings || [],
+          },
+          field_config: {
+            field_type: 'kv',
+          },
+        };
+      }),
+    };
+    console.log('noGroupOutputConfig>>', noGroupOutputConfig);
+
+    return noGroupOutputConfig;
+  };
+  defineExpose<Exposes>({
+    // 提交获取字段
+    handleGetResultConfig() {
+      console.log('getResultConfig()', getResultConfig());
+      return getResultConfig();
+    },
+  });
+</script>
+
+<style lang="postcss" scoped>
+.group-box {
+  display: flex;
+  font-size: 12px;
+  color: #4d4f56;
+}
+
+.group {
+  margin-left: 10px;
+}
+
+.info-fill {
+  margin-left: 10px;
+  font-size: 14px;
+  color: #979ba5;
+}
+
+.info-fill-tex {
+  margin-left: 10px;
+  font-size: 12px;
+  color: #979ba5;
+}
+
+.group-button {
+  margin-left: 10px;
+  font-size: 12px;
+  color: #3a84ff;
+}
+
+.plus-circle {
+  margin-left: 10px;
+  font-size: 14px;
+  color: #3a84ff;
+  cursor: pointer;
+
+}
+
+.plus-circle-tex {
+  margin-left: 5px;
+  font-size: 12px;
+  color: #3a84ff;
+  cursor: pointer;
+}
+
+.line {
+  display: inline-block;
+  width: 1px;
+  height: 16px;
+  margin-left: 10px;
+  vertical-align: middle;
+  background: #dcdee5;
+}
+</style>
