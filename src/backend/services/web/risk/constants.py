a311d33c1f16db0ba21174cac51e4a388e43cfbd
raja <1647193241@qq.com>
2025-12-23 17:06:16 +0800

    feat: 确保 RAW_EVENT_ID_REMARK 在 EventMappingFields 是一个字符串 --story=129440351
    Signed-off-by: raja <1647193241@qq.com>

--- src/backend/services/web/risk/constants.py
+++ src/backend/services/web/risk/constants.py
@@ -178,7 +178,7 @@ class EventMappingFields:
         is_index=True,
         is_dimension=False,
         property={
-            "remark": RAW_EVENT_ID_REMARK,
+            "remark": str(RAW_EVENT_ID_REMARK),
         },
     )
 
