b9d2fa793baf945782a526cadb6558a30c30f462
你的姓名[200~git config --global user.name 你的姓名^? <zzl15727777247@163.com>
2025-12-23 18:10:16 +0800

    fix: 表格执行参数 --bug=128407997

--- src/frontend/src/views/tools/tools-square/components/data-search-result.vue
+++ src/frontend/src/views/tools/tools-square/components/data-search-result.vue
@@ -108,7 +108,8 @@
       params: {
         tool_variables: props.searchList.map(item => ({
           raw_name: item.raw_name,
-          value: item.value,
+          // eslint-disable-next-line no-nested-ternary
+          value: (item.field_category === 'person_select') ? (item.value.length === 0 ?  '' :  item.value.join(','))  :  item.value,
         })),
         page: pagination.value.current,
         page_size: pagination.value.limit,
