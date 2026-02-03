47d08f190f409c08f0a3f95592f22173601be4fb
zhaoyiming111 <1632598418@qq.com>
2026-02-03 10:00:30 +0800

    fix: 审计风险页面--修复命中策略ID丢失 --bug=130801381

--- src/frontend/src/views/risk-manage/detail/components/link-event.vue
+++ src/frontend/src/views/risk-manage/detail/components/link-event.vue
@@ -110,7 +110,7 @@
                           text
                           theme="primary"
                           @click="handlerStrategy()">
-                          {{ strategyList.find((item: any) => item.value === eventItem.strategy_id)?.label }}
+                          {{ strategyList.find((item: any) => item.value === eventItem.strategy_id)?.label }} ({{ eventItem.strategy_id }})
                         </bk-button>
                         <span v-else> -- </span>
                         <audit-icon
