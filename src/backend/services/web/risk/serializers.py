aaaf0f1e39d6528871239a1b81b4b9fa703d89bc
JetDrag <taptube@gmail.com>
2026-02-26 11:56:51 +0800

    feat: 事件调查报告二期优化-风险列表增加是否生成调查报告列 --story=131028441

--- src/backend/services/web/risk/serializers.py
+++ src/backend/services/web/risk/serializers.py
@@ -434,6 +434,7 @@ class ListRiskRequestSerializer(serializers.Serializer):
     )
     title = serializers.CharField(label=gettext_lazy("Risk Title"), allow_blank=True, required=False)
     event_filters = EventFieldFilterItemSerializer(label=gettext_lazy("关联事件字段筛选"), many=True, required=False)
+    has_report = serializers.BooleanField(label=gettext_lazy("是否已生成报告"), required=False, allow_null=True)
 
     def validate(self, attrs: dict) -> dict:
         # 校验
@@ -488,7 +489,15 @@ class ListRiskRequestSerializer(serializers.Serializer):
         data["use_bkbase"] = bool(data.get("use_bkbase", False))
         # 格式转换
         for key, val in attrs.items():
-            if key in ["event_time__gte", "event_time__lt", "order_type", "order_field", "use_bkbase", "event_filters"]:
+            if key in [
+                "event_time__gte",
+                "event_time__lt",
+                "order_type",
+                "order_field",
+                "use_bkbase",
+                "event_filters",
+                "has_report",
+            ]:
                 continue
             if key in ["tag_objs__in"]:
                 data[key] = [int(i) for i in val.split(",") if i]
