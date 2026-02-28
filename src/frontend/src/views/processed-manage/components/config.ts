ca894d36c802db0dcdc184c9001a557bf59999ee
zhaoyiming111 <1632598418@qq.com>
2026-02-28 11:13:51 +0800

    feat: 处理历史--添加事件调查报告筛选项 --story=131788529

--- src/frontend/src/views/processed-manage/components/config.ts
+++ src/frontend/src/views/processed-manage/components/config.ts
@@ -96,4 +96,20 @@ export default {
     type: 'user-selector',
     required: false,
   },
+  has_report: {
+    label: '事件调查报告',
+    type: 'select',
+    required: false,
+    multiple: false,
+    service: () => Promise.resolve([
+      {
+        id: 'true',
+        name: '已生成',
+      },
+      {
+        id: 'false',
+        name: '未生成',
+      },
+    ]),
+  },
 } as Record<string, IFieldConfig>;
