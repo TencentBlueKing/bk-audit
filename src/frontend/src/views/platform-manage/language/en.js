eb72497de29b7dc4fbda69d336804393d4a3ac2e
zzl <zzl15727777247@163.com>
2026-03-30 18:11:09 +0800

    feat: 审计报表--前端页面 --story=133104895 # Reviewed, transaction id: 77175

--- src/frontend/src/views/platform-manage/language/en.js
+++ src/frontend/src/views/platform-manage/language/en.js
@@ -17,5 +17,29 @@ import { PlatformManage } from '@views/platform-manage/routes';
 */
 export default {
   platformManage: {
+    // Report Config
+    新建报表: 'New Report',
+    报表ID: 'Report ID',
+    报表名称: 'Report Name',
+    描述: 'Description',
+    'BKVision 报表': 'BKVision Report',
+    启用状态: 'Status',
+    更新人: 'Updated By',
+    更新时间: 'Updated At',
+    操作: 'Actions',
+    编辑: 'Edit',
+    启用: 'Enable',
+    停用: 'Disable',
+    删除: 'Delete',
+    取消: 'Cancel',
+    // Delete Confirm Dialog
+    '确定删除该报告？': 'Confirm to delete this report?',
+    删除的报告将: 'The deleted report will be ',
+    无法找回: 'unrecoverable',
+    '，请谨慎操作！': ', please proceed with caution!',
+    请输入报表名称: 'Please enter the report name ',
+    以确认删除: ' to confirm deletion',
+    请输入待删除的报表名称: 'Please enter the report name to delete',
+    复制成功: 'Copied successfully',
   },
 };
