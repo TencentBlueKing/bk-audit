4b7fc84bcda0f06e39056b929e65489df4f4c5cb
zhaoyiming111 <1632598418@qq.com>
2026-05-07 17:36:00 +0800

    fix: 用户画像--添加中文参数映射--bug=134065118

--- src/frontend/src/views/tools/tools-square/components/game/game-search-fields.ts
+++ src/frontend/src/views/tools/tools-square/components/game/game-search-fields.ts
@@ -16,10 +16,19 @@
 */
 import { useI18n } from 'vue-i18n';
 
+import {
+  CHAT_DETAIL_FIELDS,
+  COIN_DETAIL_FIELDS,
+  GIFT_DETAIL_FIELDS,
+  LOGIN_DETAIL_FIELDS,
+  TRADE_DETAIL_FIELDS,
+} from './game-field-keys';
+
 // 搜索字段配置接口
 export interface SearchFieldItem {
   name: string;       // 显示名称
-  id: string;         // 字段标识
+  id: string;         // 字段标识（英文）
+  fieldKey: string;   // 后端数据字段名（用于匹配行数据的 key）
   placeholder?: string;
   children?: Array<{ id: string; name: string }>;
   onlyRecommendChildren?: boolean;
@@ -61,67 +70,67 @@ export const useGameSearchFields = () => {
 
   // ========== 登录记录搜索字段 ==========
   const loginSearchFields: SearchFieldItem[] = [
-    { id: '登录IP', name: t('登录IP'), placeholder: t('请输入登录IP'), conditions: textConditions },
-    { id: '登录地点', name: t('登录地点'), placeholder: t('请输入登录地点，多个值之间用逗号分隔'), conditions: textConditions },
-    { id: '大区', name: t('大区ID'), placeholder: t('请输入大区ID'), conditions: numberConditions },
-    { id: '角色ID', name: t('角色ID'), placeholder: t('请输入角色ID'), conditions: numberConditions },
-    { id: '角色名称', name: t('角色名'), placeholder: t('请输入角色名'), conditions: textConditions },
-    { id: '等级', name: t('等级'), placeholder: t('请输入等级'), conditions: numberConditions },
-    { id: '登录设备', name: t('登录设备'), placeholder: t('请输入登录设备'), conditions: textConditions },
-    { id: '机型', name: t('机型'), placeholder: t('请输入机型'), conditions: textConditions },
+    { id: 'login_ip', fieldKey: LOGIN_DETAIL_FIELDS.LOGIN_IP, name: t('登录IP'), placeholder: t('请输入登录IP'), conditions: textConditions },
+    { id: 'login_location', fieldKey: LOGIN_DETAIL_FIELDS.LOGIN_LOCATION, name: t('登录地点'), placeholder: t('请输入登录地点，多个值之间用逗号分隔'), conditions: textConditions },
+    { id: 'zone_id', fieldKey: LOGIN_DETAIL_FIELDS.ZONE, name: t('大区ID'), placeholder: t('请输入大区ID'), conditions: numberConditions },
+    { id: 'role_id', fieldKey: LOGIN_DETAIL_FIELDS.ROLE_ID, name: t('角色ID'), placeholder: t('请输入角色ID'), conditions: numberConditions },
+    { id: 'role_name', fieldKey: LOGIN_DETAIL_FIELDS.ROLE_NAME, name: t('角色名'), placeholder: t('请输入角色名'), conditions: textConditions },
+    { id: 'level', fieldKey: LOGIN_DETAIL_FIELDS.LEVEL, name: t('等级'), placeholder: t('请输入等级'), conditions: numberConditions },
+    { id: 'login_device', fieldKey: LOGIN_DETAIL_FIELDS.LOGIN_DEVICE, name: t('登录设备'), placeholder: t('请输入登录设备'), conditions: textConditions },
+    { id: 'device_model', fieldKey: LOGIN_DETAIL_FIELDS.DEVICE_MODEL, name: t('机型'), placeholder: t('请输入机型'), conditions: textConditions },
   ];
 
   // ========== 赠送记录搜索字段 ==========
   const giftSearchFields: SearchFieldItem[] = [
-    { id: '对方openid', name: t('赠送对象'), placeholder: t('请输入赠送对象openid'), conditions: textConditions },
-    { id: '昵称', name: t('昵称'), placeholder: t('请输入昵称'), conditions: textConditions },
-    { id: '是否员工', name: t('是否员工'), placeholder: t('请选择'), conditions: enumConditions,
-      children: [{ id: '是', name: t('是') }, { id: '否', name: t('否') }], onlyRecommendChildren: true },
-    { id: '大区', name: t('大区'), placeholder: t('请输入大区'), conditions: numberConditions },
-    { id: '道具id', name: t('道具ID'), placeholder: t('请输入道具ID'), conditions: numberConditions },
-    { id: '道具名称', name: t('道具名称'), placeholder: t('请输入道具名称'), conditions: textConditions },
-    { id: '赠送金额', name: t('赠送总额'), placeholder: t('请输入赠送总额'), conditions: numberConditions },
-    { id: '赠送单价', name: t('赠送单价'), placeholder: t('请输入赠送单价'), conditions: numberConditions },
-    { id: '道具数量', name: t('赠送数量'), placeholder: t('请输入赠送数量'), conditions: numberConditions },
-    { id: '赠送次数', name: t('赠送次数'), placeholder: t('请输入赠送次数'), conditions: numberConditions },
+    { id: 'target_openid', fieldKey: GIFT_DETAIL_FIELDS.TARGET_OPENID, name: t('赠送对象'), placeholder: t('请输入赠送对象openid'), conditions: textConditions },
+    { id: 'nickname', fieldKey: GIFT_DETAIL_FIELDS.NICKNAME, name: t('昵称'), placeholder: t('请输入昵称'), conditions: textConditions },
+    { id: 'is_employee', fieldKey: GIFT_DETAIL_FIELDS.IS_EMPLOYEE, name: t('是否员工'), placeholder: t('请选择'), conditions: enumConditions,
+      children: [{ id: 'yes', name: t('是') }, { id: 'no', name: t('否') }], onlyRecommendChildren: true },
+    { id: 'zone_id', fieldKey: GIFT_DETAIL_FIELDS.ZONE, name: t('大区'), placeholder: t('请输入大区'), conditions: numberConditions },
+    { id: 'item_id', fieldKey: GIFT_DETAIL_FIELDS.ITEM_ID, name: t('道具ID'), placeholder: t('请输入道具ID'), conditions: numberConditions },
+    { id: 'item_name', fieldKey: GIFT_DETAIL_FIELDS.ITEM_NAME, name: t('道具名称'), placeholder: t('请输入道具名称'), conditions: textConditions },
+    { id: 'gift_amount', fieldKey: GIFT_DETAIL_FIELDS.GIFT_AMOUNT, name: t('赠送总额'), placeholder: t('请输入赠送总额'), conditions: numberConditions },
+    { id: 'gift_unit_price', fieldKey: GIFT_DETAIL_FIELDS.GIFT_UNIT_PRICE, name: t('赠送单价'), placeholder: t('请输入赠送单价'), conditions: numberConditions },
+    { id: 'item_count', fieldKey: GIFT_DETAIL_FIELDS.ITEM_COUNT, name: t('赠送数量'), placeholder: t('请输入赠送数量'), conditions: numberConditions },
+    { id: 'gift_times', fieldKey: GIFT_DETAIL_FIELDS.GIFT_TIMES, name: t('赠送次数'), placeholder: t('请输入赠送次数'), conditions: numberConditions },
   ];
 
   // ========== 交易记录搜索字段 ==========
   const tradeSearchFields: SearchFieldItem[] = [
-    { id: '交易对象', name: t('交易对象'), placeholder: t('请输入交易对象'), conditions: textConditions },
-    { id: '昵称', name: t('昵称'), placeholder: t('请输入昵称'), conditions: textConditions },
-    { id: '是否员工', name: t('是否员工'), placeholder: t('请选择'), conditions: enumConditions,
-      children: [{ id: '是', name: t('是') }, { id: '否', name: t('否') }], onlyRecommendChildren: true },
-    { id: '大区ID', name: t('大区'), placeholder: t('请输入大区ID'), conditions: numberConditions },
-    { id: '道具id', name: t('道具ID'), placeholder: t('请输入道具ID'), conditions: numberConditions },
-    { id: '道具名称', name: t('道具名称'), placeholder: t('请输入道具名称'), conditions: textConditions },
-    { id: '交易总额', name: t('交易总额'), placeholder: t('请输入交易总额'), conditions: numberConditions },
-    { id: '交易单价', name: t('交易单价'), placeholder: t('请输入交易单价'), conditions: numberConditions },
-    { id: '交易数量', name: t('交易数量'), placeholder: t('请输入交易数量'), conditions: numberConditions },
-    { id: '交易次数', name: t('交易次数'), placeholder: t('请输入交易次数'), conditions: numberConditions },
+    { id: 'trade_target', fieldKey: TRADE_DETAIL_FIELDS.TRADE_TARGET, name: t('交易对象'), placeholder: t('请输入交易对象'), conditions: textConditions },
+    { id: 'nickname', fieldKey: TRADE_DETAIL_FIELDS.NICKNAME, name: t('昵称'), placeholder: t('请输入昵称'), conditions: textConditions },
+    { id: 'is_employee', fieldKey: TRADE_DETAIL_FIELDS.IS_EMPLOYEE, name: t('是否员工'), placeholder: t('请选择'), conditions: enumConditions,
+      children: [{ id: 'yes', name: t('是') }, { id: 'no', name: t('否') }], onlyRecommendChildren: true },
+    { id: 'zone_id', fieldKey: TRADE_DETAIL_FIELDS.ZONE_ID, name: t('大区'), placeholder: t('请输入大区ID'), conditions: numberConditions },
+    { id: 'item_id', fieldKey: TRADE_DETAIL_FIELDS.ITEM_ID, name: t('道具ID'), placeholder: t('请输入道具ID'), conditions: numberConditions },
+    { id: 'item_name', fieldKey: TRADE_DETAIL_FIELDS.ITEM_NAME, name: t('道具名称'), placeholder: t('请输入道具名称'), conditions: textConditions },
+    { id: 'trade_amount', fieldKey: TRADE_DETAIL_FIELDS.TRADE_AMOUNT, name: t('交易总额'), placeholder: t('请输入交易总额'), conditions: numberConditions },
+    { id: 'trade_unit_price', fieldKey: TRADE_DETAIL_FIELDS.TRADE_UNIT_PRICE, name: t('交易单价'), placeholder: t('请输入交易单价'), conditions: numberConditions },
+    { id: 'trade_count', fieldKey: TRADE_DETAIL_FIELDS.TRADE_COUNT, name: t('交易数量'), placeholder: t('请输入交易数量'), conditions: numberConditions },
+    { id: 'trade_times', fieldKey: TRADE_DETAIL_FIELDS.TRADE_TIMES, name: t('交易次数'), placeholder: t('请输入交易次数'), conditions: numberConditions },
   ];
 
   // ========== 代币发放记录搜索字段 ==========
   const coinSearchFields: SearchFieldItem[] = [
-    { id: '发放人', name: t('发放人'), placeholder: t('请输入发放人'), conditions: textConditions },
-    { id: '大区', name: t('大区'), placeholder: t('请输入大区'), conditions: numberConditions },
-    { id: '发放数量', name: t('发放数量'), placeholder: t('请输入发放数量'), conditions: numberConditions },
-    { id: '发放金额（元）', name: t('发放金额'), placeholder: t('请输入发放金额'), conditions: numberConditions },
-    { id: '操作原因', name: t('操作原因'), placeholder: t('请输入操作原因'), conditions: textConditions },
-    { id: '需求类型', name: t('需求类型'), placeholder: t('请输入需求类型'), conditions: textConditions },
-    { id: '需求来源', name: t('需求来源'), placeholder: t('请输入需求来源'), conditions: textConditions },
+    { id: 'issuer', fieldKey: COIN_DETAIL_FIELDS.ISSUER, name: t('发放人'), placeholder: t('请输入发放人'), conditions: textConditions },
+    { id: 'zone_id', fieldKey: COIN_DETAIL_FIELDS.ZONE, name: t('大区'), placeholder: t('请输入大区'), conditions: numberConditions },
+    { id: 'issue_count', fieldKey: COIN_DETAIL_FIELDS.ISSUE_COUNT, name: t('发放数量'), placeholder: t('请输入发放数量'), conditions: numberConditions },
+    { id: 'issue_amount', fieldKey: COIN_DETAIL_FIELDS.ISSUE_AMOUNT, name: t('发放金额'), placeholder: t('请输入发放金额'), conditions: numberConditions },
+    { id: 'operation_reason', fieldKey: COIN_DETAIL_FIELDS.OPERATION_REASON, name: t('操作原因'), placeholder: t('请输入操作原因'), conditions: textConditions },
+    { id: 'demand_type', fieldKey: COIN_DETAIL_FIELDS.DEMAND_TYPE, name: t('需求类型'), placeholder: t('请输入需求类型'), conditions: textConditions },
+    { id: 'demand_source', fieldKey: COIN_DETAIL_FIELDS.DEMAND_SOURCE, name: t('需求来源'), placeholder: t('请输入需求来源'), conditions: textConditions },
   ];
 
   // ========== 聊天记录搜索字段 ==========
   const chatSearchFields: SearchFieldItem[] = [
-    { id: '发起方', name: t('发起方'), placeholder: t('请输入发起方'), conditions: textConditions },
-    { id: '接收方openid', name: t('接收方openid'), placeholder: t('请输入接收方openid'), conditions: textConditions },
-    { id: '接收方昵称', name: t('接收方昵称'), placeholder: t('请输入接收方昵称'), conditions: textConditions },
-    { id: '聊天内容', name: t('聊天内容'), placeholder: t('请输入聊天内容'), conditions: textConditions },
-    { id: '大区ID', name: t('大区'), placeholder: t('请输入大区ID'), conditions: numberConditions },
-    { id: '是否员工', name: t('是否员工'), placeholder: t('请选择'), conditions: enumConditions,
-      children: [{ id: '是', name: t('是') }, { id: '否', name: t('否') }], onlyRecommendChildren: true },
-    { id: '信息类型', name: t('信息类型'), placeholder: t('请输入信息类型'), conditions: textConditions },
+    { id: 'sender', fieldKey: CHAT_DETAIL_FIELDS.SENDER, name: t('发起方'), placeholder: t('请输入发起方'), conditions: textConditions },
+    { id: 'receiver_openid', fieldKey: CHAT_DETAIL_FIELDS.RECEIVER_OPENID, name: t('接收方openid'), placeholder: t('请输入接收方openid'), conditions: textConditions },
+    { id: 'receiver_nickname', fieldKey: CHAT_DETAIL_FIELDS.RECEIVER_NICKNAME, name: t('接收方昵称'), placeholder: t('请输入接收方昵称'), conditions: textConditions },
+    { id: 'chat_content', fieldKey: CHAT_DETAIL_FIELDS.CHAT_CONTENT, name: t('聊天内容'), placeholder: t('请输入聊天内容'), conditions: textConditions },
+    { id: 'zone_id', fieldKey: CHAT_DETAIL_FIELDS.ZONE_ID, name: t('大区'), placeholder: t('请输入大区ID'), conditions: numberConditions },
+    { id: 'is_employee', fieldKey: CHAT_DETAIL_FIELDS.IS_EMPLOYEE, name: t('是否员工'), placeholder: t('请选择'), conditions: enumConditions,
+      children: [{ id: 'yes', name: t('是') }, { id: 'no', name: t('否') }], onlyRecommendChildren: true },
+    { id: 'message_type', fieldKey: CHAT_DETAIL_FIELDS.MESSAGE_TYPE, name: t('信息类型'), placeholder: t('请输入信息类型'), conditions: textConditions },
   ];
 
   return {
--- src/frontend/src/views/tools/tools-square/components/game/game-table-columns.ts
+++ src/frontend/src/views/tools/tools-square/components/game/game-table-columns.ts
@@ -16,6 +16,16 @@
 */
 import { useI18n } from 'vue-i18n';
 
+import {
+  CHAT_DETAIL_FIELDS,
+  COIN_DETAIL_FIELDS,
+  GIFT_DETAIL_FIELDS,
+  LOGIN_DETAIL_FIELDS,
+  LOGIN_STAT_FIELDS,
+  ROLE_FIELDS,
+  TRADE_DETAIL_FIELDS,
+} from './game-field-keys';
+
 /**
  * 创建所有游戏详情页的表格列配置
  */
@@ -24,92 +34,92 @@ export const useGameTableColumns = () => {
 
   // ========== 概览 - 角色总览列 ==========
   const roleColumns = [
-    { label: () => t('角色名称'), field: '角色名称' },
-    { label: () => t('角色ID'), field: '角色ID' },
-    { label: () => t('平台'), field: '平台' },
-    { label: () => t('大区'), field: '大区' },
-    { label: () => `${t('登录地点数')}/${t('月')}`, field: '登录地点（月）', sort: true },
-    { label: () => `${t('登录地点数')}/${t('年')}`, field: '登录地点（年）', sort: true },
-    { label: () => `${t('登录次数')}/${t('月')}`, field: '登录次数（月）', sort: true },
-    { label: () => `${t('登录次数')}/${t('年')}`, field: '登录次数（年）', sort: true },
-    { label: () => `${t('交易对象数')}/${t('月')}`, field: '交易对象（月）', sort: true },
-    { label: () => `${t('交易对象数')}/${t('年')}`, field: '交易对象（年）', sort: true },
+    { label: () => t('角色名称'), field: ROLE_FIELDS.ROLE_NAME },
+    { label: () => t('角色ID'), field: ROLE_FIELDS.ROLE_ID },
+    { label: () => t('平台'), field: ROLE_FIELDS.PLATFORM },
+    { label: () => t('大区'), field: ROLE_FIELDS.ZONE },
+    { label: () => `${t('登录地点数')}/${t('月')}`, field: ROLE_FIELDS.LOGIN_LOCATION_MONTH, sort: true },
+    { label: () => `${t('登录地点数')}/${t('年')}`, field: ROLE_FIELDS.LOGIN_LOCATION_YEAR, sort: true },
+    { label: () => `${t('登录次数')}/${t('月')}`, field: ROLE_FIELDS.LOGIN_COUNT_MONTH, sort: true },
+    { label: () => `${t('登录次数')}/${t('年')}`, field: ROLE_FIELDS.LOGIN_COUNT_YEAR, sort: true },
+    { label: () => `${t('交易对象数')}/${t('月')}`, field: ROLE_FIELDS.TRADE_TARGET_MONTH, sort: true },
+    { label: () => `${t('交易对象数')}/${t('年')}`, field: ROLE_FIELDS.TRADE_TARGET_YEAR, sort: true },
   ];
 
   // ========== 概览 - 登录统计列 ==========
   const loginStatColumns = [
-    { label: () => t('统计频率'), field: 'days_class' },
-    { label: () => t('登录次数'), field: '登录次数', sort: true },
-    { label: () => t('登录地点数'), field: '登录地点数', sort: true },
-    { label: () => t('登录设备数'), field: '登录设备数', sort: true },
-    { label: () => `${t('登录IP')}${t('数')}`, field: '登录IP数', sort: true },
+    { label: () => t('统计频率'), field: LOGIN_STAT_FIELDS.DAYS_CLASS },
+    { label: () => t('登录次数'), field: LOGIN_STAT_FIELDS.LOGIN_COUNT, sort: true },
+    { label: () => t('登录地点数'), field: LOGIN_STAT_FIELDS.LOGIN_LOCATION_COUNT, sort: true },
+    { label: () => t('登录设备数'), field: LOGIN_STAT_FIELDS.LOGIN_DEVICE_COUNT, sort: true },
+    { label: () => `${t('登录IP')}${t('数')}`, field: LOGIN_STAT_FIELDS.LOGIN_IP_COUNT, sort: true },
   ];
 
   // ========== 登录记录明细列 ==========
   const loginDetailColumns = [
-    { label: () => t('登录时间'), field: '登录时间', sort: true },
-    { label: () => t('登录地点'), field: '登录地点', filter: true },
-    { label: () => t('登录IP'), field: '登录IP' },
-    { label: () => t('大区'), field: '大区' },
-    { label: () => t('角色ID'), field: '角色ID' },
-    { label: () => t('角色名称'), field: '角色名称' },
-    { label: () => t('等级'), field: '等级' },
-    { label: () => t('登录设备'), field: '登录设备', filter: true },
-    { label: () => t('机型'), field: '机型', filter: true },
+    { label: () => t('登录时间'), field: LOGIN_DETAIL_FIELDS.LOGIN_TIME, sort: true },
+    { label: () => t('登录地点'), field: LOGIN_DETAIL_FIELDS.LOGIN_LOCATION, filter: true },
+    { label: () => t('登录IP'), field: LOGIN_DETAIL_FIELDS.LOGIN_IP },
+    { label: () => t('大区'), field: LOGIN_DETAIL_FIELDS.ZONE },
+    { label: () => t('角色ID'), field: LOGIN_DETAIL_FIELDS.ROLE_ID },
+    { label: () => t('角色名称'), field: LOGIN_DETAIL_FIELDS.ROLE_NAME },
+    { label: () => t('等级'), field: LOGIN_DETAIL_FIELDS.LEVEL },
+    { label: () => t('登录设备'), field: LOGIN_DETAIL_FIELDS.LOGIN_DEVICE, filter: true },
+    { label: () => t('机型'), field: LOGIN_DETAIL_FIELDS.DEVICE_MODEL, filter: true },
   ];
 
   // ========== 赠送记录明细列 ==========
   const giveDetailColumns = [
-    { label: () => t('赠送对象'), field: '对方openid' },
-    { label: () => t('昵称'), field: '昵称' },
-    { label: () => t('是否员工'), field: '是否员工' },
-    { label: () => t('赠送时间'), field: '时间', sort: true },
-    { label: () => t('大区'), field: '大区' },
-    { label: () => t('道具ID'), field: '道具id' },
-    { label: () => t('道具名称'), field: '道具名称' },
-    { label: () => `${t('赠送总额')}(${t('元')})`, field: '赠送金额', sort: true },
-    { label: () => `${t('赠送单价')}(${t('元')})`, field: '赠送单价', sort: true },
-    { label: () => t('赠送数量'), field: '道具数量', sort: true },
-    { label: () => t('赠送次数'), field: '赠送次数', sort: true },
+    { label: () => t('赠送对象'), field: GIFT_DETAIL_FIELDS.TARGET_OPENID },
+    { label: () => t('昵称'), field: GIFT_DETAIL_FIELDS.NICKNAME },
+    { label: () => t('是否员工'), field: GIFT_DETAIL_FIELDS.IS_EMPLOYEE },
+    { label: () => t('赠送时间'), field: GIFT_DETAIL_FIELDS.TIME, sort: true },
+    { label: () => t('大区'), field: GIFT_DETAIL_FIELDS.ZONE },
+    { label: () => t('道具ID'), field: GIFT_DETAIL_FIELDS.ITEM_ID },
+    { label: () => t('道具名称'), field: GIFT_DETAIL_FIELDS.ITEM_NAME },
+    { label: () => `${t('赠送总额')}(${t('元')})`, field: GIFT_DETAIL_FIELDS.GIFT_AMOUNT, sort: true },
+    { label: () => `${t('赠送单价')}(${t('元')})`, field: GIFT_DETAIL_FIELDS.GIFT_UNIT_PRICE, sort: true },
+    { label: () => t('赠送数量'), field: GIFT_DETAIL_FIELDS.ITEM_COUNT, sort: true },
+    { label: () => t('赠送次数'), field: GIFT_DETAIL_FIELDS.GIFT_TIMES, sort: true },
   ];
 
   // ========== 交易记录明细列 ==========
   const dealDetailColumns = [
-    { label: () => t('交易对象'), field: '交易对象' },
-    { label: () => t('昵称'), field: '昵称' },
-    { label: () => t('是否员工'), field: '是否员工' },
-    { label: () => t('交易时间'), field: '交易时间', sort: true },
-    { label: () => t('大区'), field: '大区ID' },
-    { label: () => t('道具ID'), field: '道具id' },
-    { label: () => t('道具名称'), field: '道具名称' },
-    { label: () => `${t('交易总额')}(${t('元')})`, field: '交易总额', sort: true },
-    { label: () => `${t('交易单价')}(${t('元')})`, field: '交易单价', sort: true },
-    { label: () => t('交易数量'), field: '交易数量', sort: true },
-    { label: () => t('交易次数'), field: '交易次数', sort: true },
+    { label: () => t('交易对象'), field: TRADE_DETAIL_FIELDS.TRADE_TARGET },
+    { label: () => t('昵称'), field: TRADE_DETAIL_FIELDS.NICKNAME },
+    { label: () => t('是否员工'), field: TRADE_DETAIL_FIELDS.IS_EMPLOYEE },
+    { label: () => t('交易时间'), field: TRADE_DETAIL_FIELDS.TRADE_TIME, sort: true },
+    { label: () => t('大区'), field: TRADE_DETAIL_FIELDS.ZONE_ID },
+    { label: () => t('道具ID'), field: TRADE_DETAIL_FIELDS.ITEM_ID },
+    { label: () => t('道具名称'), field: TRADE_DETAIL_FIELDS.ITEM_NAME },
+    { label: () => `${t('交易总额')}(${t('元')})`, field: TRADE_DETAIL_FIELDS.TRADE_AMOUNT, sort: true },
+    { label: () => `${t('交易单价')}(${t('元')})`, field: TRADE_DETAIL_FIELDS.TRADE_UNIT_PRICE, sort: true },
+    { label: () => t('交易数量'), field: TRADE_DETAIL_FIELDS.TRADE_COUNT, sort: true },
+    { label: () => t('交易次数'), field: TRADE_DETAIL_FIELDS.TRADE_TIMES, sort: true },
   ];
 
   // ========== 代币发放记录明细列 ==========
   const sapDetailColumns = [
-    { label: () => t('发放时间'), field: '发放时间', sort: true },
-    { label: () => t('发放人'), field: '发放人' },
-    { label: () => t('大区'), field: '大区' },
-    { label: () => t('发放数量'), field: '发放数量', sort: true },
-    { label: () => `${t('发放金额')}(${t('元')})`, field: '发放金额（元）', sort: true },
-    { label: () => t('操作原因'), field: '操作原因' },
-    { label: () => t('需求类型'), field: '需求类型', filter: true },
-    { label: () => t('需求来源'), field: '需求来源', filter: true },
+    { label: () => t('发放时间'), field: COIN_DETAIL_FIELDS.ISSUE_TIME, sort: true },
+    { label: () => t('发放人'), field: COIN_DETAIL_FIELDS.ISSUER },
+    { label: () => t('大区'), field: COIN_DETAIL_FIELDS.ZONE },
+    { label: () => t('发放数量'), field: COIN_DETAIL_FIELDS.ISSUE_COUNT, sort: true },
+    { label: () => `${t('发放金额')}(${t('元')})`, field: COIN_DETAIL_FIELDS.ISSUE_AMOUNT, sort: true },
+    { label: () => t('操作原因'), field: COIN_DETAIL_FIELDS.OPERATION_REASON },
+    { label: () => t('需求类型'), field: COIN_DETAIL_FIELDS.DEMAND_TYPE, filter: true },
+    { label: () => t('需求来源'), field: COIN_DETAIL_FIELDS.DEMAND_SOURCE, filter: true },
   ];
 
   // ========== 聊天记录明细列 ==========
   const chatDetailColumns = [
-    { label: () => t('发起方'), field: '发起方' },
-    { label: () => t('接收方openid'), field: '接收方openid' },
-    { label: () => t('接收方昵称'), field: '接收方昵称' },
-    { label: () => t('聊天时间'), field: '聊天时间', sort: true },
-    { label: () => t('聊天内容'), field: '聊天内容' },
-    { label: () => t('大区'), field: '大区ID' },
-    { label: () => t('是否员工'), field: '是否员工', filter: true },
-    { label: () => t('信息类型'), field: '信息类型', filter: true },
+    { label: () => t('发起方'), field: CHAT_DETAIL_FIELDS.SENDER },
+    { label: () => t('接收方openid'), field: CHAT_DETAIL_FIELDS.RECEIVER_OPENID },
+    { label: () => t('接收方昵称'), field: CHAT_DETAIL_FIELDS.RECEIVER_NICKNAME },
+    { label: () => t('聊天时间'), field: CHAT_DETAIL_FIELDS.CHAT_TIME, sort: true },
+    { label: () => t('聊天内容'), field: CHAT_DETAIL_FIELDS.CHAT_CONTENT },
+    { label: () => t('大区'), field: CHAT_DETAIL_FIELDS.ZONE_ID },
+    { label: () => t('是否员工'), field: CHAT_DETAIL_FIELDS.IS_EMPLOYEE, filter: true },
+    { label: () => t('信息类型'), field: CHAT_DETAIL_FIELDS.MESSAGE_TYPE, filter: true },
   ];
 
   return {
