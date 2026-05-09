3f68de0a549ea193efb9b3daa7bb89f200ac8afe
zhaoyiming111 <1632598418@qq.com>
2026-05-09 18:35:54 +0800

    fix: 场景配置-获取场景下有权限的数据表看起来没有过滤 --bug=134142846

--- src/frontend/src/views/link-data-manage/link-data-create/components/links.vue
+++ src/frontend/src/views/link-data-manage/link-data-create/components/links.vue
@@ -332,7 +332,7 @@
   // 获取tableid
   const {
     run: fetchTable,
-  } = useRequest(StrategyManageService.fetchTable, {
+  } = useRequest(StrategyManageService.fetchScenePermissionTable, {
     defaultValue: [],
   });
 
