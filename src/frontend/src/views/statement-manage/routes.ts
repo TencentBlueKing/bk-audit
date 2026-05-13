c0ce644359ebe7a382f8dc66f156a2967afec9f4
zzl <zzl15727777247@163.com>
2026-05-13 21:28:49 +0800

    feat: 侧边栏不支持收起  --story=134237040 # Reviewed, transaction id: 80151

--- src/frontend/src/views/statement-manage/routes.ts
+++ src/frontend/src/views/statement-manage/routes.ts
@@ -29,7 +29,7 @@ export default {
       name: 'statementManageDetail',
       meta: {
         title: '',
-        // skeleton: 'noticeGroupList',
+        hideToggleBtn: true,
       },
     },
   ],
