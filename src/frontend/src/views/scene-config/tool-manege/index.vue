0d2ef6a4258e384d28aa570b8bbebf66fff18ed3
zzl <zzl15727777247@163.com>
2026-05-25 15:30:42 +0800

    feat: 修改tag 样式  --story=134237040

--- src/frontend/src/views/scene-config/tool-manege/index.vue
+++ src/frontend/src/views/scene-config/tool-manege/index.vue
@@ -55,7 +55,7 @@
               {{ t('启用') }}
               <bk-tag
                 class="status-count"
-                theme="success">
+                theme="info">
                 {{ statusCounts.published }}
               </bk-tag>
             </bk-radio-button>
@@ -67,7 +67,7 @@
               {{ t('停用') }}
               <bk-tag
                 class="status-count"
-                theme="warning">
+                theme="info">
                 {{ statusCounts.unpublished }}
               </bk-tag>
             </bk-radio-button>
