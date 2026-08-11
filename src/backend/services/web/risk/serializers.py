1e843dde654c43dc4ab08c4a095daa37680ae4bf
WuFantaotao <1471868238@qq.com>
2026-08-11 15:52:00 +0800

    fix: 修正基本事件字段的type标识的序列化字段 --story=136973746

--- src/backend/services/web/risk/serializers.py
+++ src/backend/services/web/risk/serializers.py
@@ -380,6 +380,7 @@ class ListEventFieldsByStrategyResponseSerializer(serializers.Serializer):
     field_name = serializers.CharField(label=gettext_lazy("字段名"))
     display_name = serializers.CharField(label=gettext_lazy("字段显示名"))
     id = serializers.CharField(label=gettext_lazy("字段ID"))
+    type = serializers.CharField(label=gettext_lazy("事件字段类型"), required=False, allow_null=True)
 
 
 class RiskEventSubscriptionQuerySerializer(serializers.Serializer):
