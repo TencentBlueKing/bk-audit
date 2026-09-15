1dee79b2ccec5011cc83afde9ed6d9f0578b3c9f
zzl <zzl15727777247@163.com>
2026-09-15 14:58:38 +0800

    feat: 修改工具名称显示 --story=138020744

--- src/frontend/src/views/strategy-manage/strategy-create/components/step2/components/event-table/index.vue
+++ src/frontend/src/views/strategy-manage/strategy-create/components/step2/components/event-table/index.vue
@@ -98,8 +98,8 @@
 
   import useRequest from '@/hooks/use-request';
   import { useToolDialog } from '@/hooks/use-tool-dialog';
-  import { getToolListScopeParams } from '@/utils/assist/scene-system-params';
   import {
+    getStrategySystemScopeParams,
     isStrategyCloneRoute,
     isStrategyEditRoute,
   } from '../../../../../utils/strategy-routes';
@@ -207,7 +207,10 @@
   });
 
 
-  const buildToolListParams = () => getToolListScopeParams({ status: 'published' });
+  const buildToolListParams = () => ({
+    ...getStrategySystemScopeParams(route),
+    status: 'published',
+  });
 
   // 获取所有工具
   const {
